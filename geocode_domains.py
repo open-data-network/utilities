dbfc7fb6c04bc7a83af4541db9e2d14dccca68ed
Tim Clemans <tim@insideyourgovernment.com>
2016-02-05 19:30:09 +0000

    fix bug

--- geocode_domains.py
+++ geocode_domains.py
@@ -5,8 +5,7 @@ import json
 with open('creds.json', 'r') as f:
     creds = json.loads(f.read())
 client = Socrata("odn.data.socrata.com", creds["token"], username=creds['username'], password=creds['password'])
-data = requests.get('https://odn.data.socrata.com/resource/jwbj-xtgt.json?$select=:*,*&$where=region_name%20IS%20NOT%20NULL%20AND%20location%20IS%20NULL').json()
-#{u'latitude': u'41.1085', u'needs_recoding': False, u'longitude': u'-117.6135'}
+data = requests.get('https://odn.data.socrata.com/resource/jwbj-xtgt.json?$where=region_name%20IS%20NOT%20NULL%20AND%20location%20IS%20NULL').json()
 for row in data:
     if len(row['region_name'].split(',')) > 2:
         location = requests.get('http://nominatim.openstreetmap.org/search/?q=%s&format=json' % (row['region_name'])).json()
